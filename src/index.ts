c871268b3dd58481cb5555231e277a295f04f36f
CheolminJeon <jcm940308@gmail.com>
2020-01-24 22:28:34 -0800

    #4 Interface in Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 /** @format */
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hi ${name}, You are ${age}. You are a ${gender}!`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: 'minimal',
+  age: 22,
+  gender: 'male'
 };
 
-console.log(sayHi('minimal', 26, 'male'));
+const sayHi = (person: Human): string => {
+  return `Hi ${person.name}, You are ${person.age}. You are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
 
 export {};
